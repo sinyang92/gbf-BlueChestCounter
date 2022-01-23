bb71360d9beb6ee28667f28c8bf449c7afe06e64
sinyang92 <svenyang92@gmail.com>
2022-01-23 21:13:34 +1100

    fix logic

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -1,10 +1,11 @@
 const regex = new RegExp("http:\/\/game\.granbluefantasy\.jp\/#result_multi\/(?!detail)[0-9]*");
 const BLUE_CHEST_CLASS_NAME = "ico-treasure-11-mini";
-const anima_id_list = ['10_41', '10_42', '10_43', '10_44', '10_45', '10_46'];
+const anima_id_list = ["10_41", "10_42", "10_43", "10_44", "10_45", "10_46"];
+var check;
 
 $(window).bind('hashchange', function() {
     if (window.location.href.match(regex)) {
-		var check = setInterval(function() {
+		check = setInterval(function() {
 
 			if (isBigBa() && document.getElementsByClassName(BLUE_CHEST_CLASS_NAME).length > 0) {
 				chrome.storage.local.get(['blueChest'], function(result){
@@ -24,15 +25,23 @@ $(window).bind('hashchange', function() {
 				});
 			}
 		}, 500);
-		
-}});
+
+	} else {
+		clearInterval(check);
+	}
+});
 
 function isBigBa() {
-	anima_id_list.forEach(id => {
-		if (document.querySelector("[data-key='" + id + "']") != undefined) {
-			return true
-		}
-	})
+	var retVal = false
 
-	return false
+	for (i = 0; i < anima_id_list.length; i++) {
+		if (document.querySelector("[data-key='" + anima_id_list[i] + "']") != undefined) {
+			console.log("侦测到大巴结算页");
+			clearInterval(check);
+			retVal = true;
+			break;
+		}
+	}
+
+	return retVal;
 }
