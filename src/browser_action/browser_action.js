ae5064b931ec86faecf58ad5b6363733587f35fb
huangtianning <huangtn@wjs.com>
2022-01-25 15:10:31 +0800

    T:m UI

--- src/browser_action/browser_action.js
+++ src/browser_action/browser_action.js
@@ -1,18 +1,58 @@
-chrome.storage.local.get(['blueChest'], function(result){
-    if (result.blueChest === undefined){
+chrome.storage.local.get(['blueChestObj'], function (result) {
+    if (!result.blueChestObj) {
         document.getElementById("blue-chest").innerHTML = 0;
+        document.getElementById("white-ring").innerHTML = 0;
+        document.getElementById("blue-ring").innerHTML = 0;
+        document.getElementById("red-ring").innerHTML = 0;
+        document.getElementById("ffj").innerHTML = 0;
+        document.getElementById("un-hit").innerHTML = 0;
+        document.getElementById("hit-array").innerHTML = '无';
     } else {
-        document.getElementById("blue-chest").innerHTML = result.blueChest;
-    }});
+        document.getElementById("blue-chest").innerHTML = result.blueChestObj.count;
+        document.getElementById("white-ring").innerHTML = result.blueChestObj.whiteRingCount;
+        document.getElementById("blue-ring").innerHTML = result.blueChestObj.blueRingCount;
+        document.getElementById("red-ring").innerHTML = result.blueChestObj.redRingCount;
+        document.getElementById("ffj").innerHTML = result.blueChestObj.ffjCount;
+        document.getElementById("un-hit").innerHTML = result.blueChestObj.unHitCount;
+        document.getElementById("hit-array").innerHTML = (result.blueChestObj.historyHitArray || []).length > 0 ? getHitStr(result.blueChestObj.historyHitArray) : '无';
+    }
+});
 
-document.addEventListener('DOMContentLoaded', function() {
+
+document.addEventListener('DOMContentLoaded', function () {
     document.getElementById("resetCounter").addEventListener("click", resetCounter);
 });
 
 function resetCounter() {
     document.getElementById("blue-chest").innerHTML = 0;
 
-    chrome.storage.local.set({'blueChest': 0}, function(){
-        console.log('蓝箱设置为： ' + 0);
+    chrome.storage.local.get(['blueChestObj'], function (result) {
+        let resetObj;
+        if (!result.blueChestObj) {
+            resetObj = {
+                count: 0,
+                ffjCount: 0,
+                whiteRingCount: 0,
+                blueRingCount: 0,
+                redRingCount: 0,
+                unHitCount: 0,
+                historyHitArray: []
+            }
+        } else {
+            resetObj = result.blueChestObj
+            resetObj.count = 0
+        }
+        chrome.storage.local.set({ 'blueChestObj': resetObj }, function () {
+            console.log('蓝箱记录已重置');
+        });
     });
+}
+
+function getHitStr(array) {
+    let str = '';
+    array.map((e, i) => {
+        str += e.v
+        if (i != array.length - 1) str += ';'
+    })
+    return str
 }
--- src/inject/inject.js
+++ src/inject/inject.js
@@ -1,28 +1,37 @@
 const regex = new RegExp("http:\/\/game\.granbluefantasy\.jp\/#result_multi\/(?!detail)[0-9]*");
 const BLUE_CHEST_CLASS_NAME = "ico-treasure-11-mini";
 const anima_id_list = ["10_41", "10_42", "10_43", "10_44", "10_45", "10_46"];
+const EMPTY_OBJ = {
+	count: 0,
+	ffjCount: 0,
+	whiteRingCount: 0,
+	blueRingCount: 0,
+	redRingCount: 0,
+	unHitCount:0,
+	historyHitArray:[]
+};
 var check;
 
-$(window).bind('hashchange', function() {
-    if (window.location.href.match(regex)) {
-		check = setInterval(function() {
-
-			if (isBigBa() && document.getElementsByClassName(BLUE_CHEST_CLASS_NAME).length > 0) {
-				chrome.storage.local.get(['blueChest'], function(result){
-					if (result.blueChest === undefined){
-						chrome.storage.local.set({'blueChest': 1}, function(){
-							console.log('蓝箱设置为： ' + 1);
-							clearInterval(check);
-						});
-
-					} else {
-						let count = result.blueChest;
-						chrome.storage.local.set({'blueChest': (count + 1)}, function(){
-							console.log('蓝箱设置为： ' + (count + 1));
-							clearInterval(check);
-						})
-					}
-				});
+$(window).bind('hashchange', function () {
+	if (window.location.href.match(regex)) {
+		check = setInterval(function () {
+			if (isBigBa()) {
+				if (document.getElementsByClassName(BLUE_CHEST_CLASS_NAME).length > 0) {
+					chrome.storage.local.get(['blueChestObj'], function (result) {
+						if (!result.blueChestObj) {
+							chrome.storage.local.set({ 'blueChestObj': getBlueChestObj() }, function () {
+								console.log('蓝箱、戒指设置初始化');
+								// clearInterval(check);
+							});
+						} else {
+							let obj = getBlueChestObj(result.blueChestObj)
+							chrome.storage.local.set({ 'blueChestObj': obj }, function () {
+								console.log('蓝箱设置为： ' + (obj.count));
+								// clearInterval(check);
+							})
+						}
+					});
+				}
 			}
 		}, 500);
 
@@ -44,4 +53,22 @@ function isBigBa() {
 	}
 
 	return retVal;
+}
+
+function getBlueChestObj(data) {
+	let obj = data || EMPTY_OBJ
+	obj.count += 1
+	obj.unHitCount += 1
+	if (document.querySelector("[data-key='17_20004']")) {
+		obj.ffjCount += 1
+		obj.historyHitArray.push({k : new Date().toLocaleString(), v : obj.unHitCount})
+		obj.unHitCount = 0
+	} else if (document.querySelector("[data-key='73_1']")) {
+		obj.whiteRingCount += 1
+	} else if (document.querySelector("[data-key='73_2']")) {
+		obj.blueRingCount += 1
+	} else if (document.querySelector("[data-key='73_2']")) {
+		obj.redRingCount += 1
+	}
+	return obj;
 }
